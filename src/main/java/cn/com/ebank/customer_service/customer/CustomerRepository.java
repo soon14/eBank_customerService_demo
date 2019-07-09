7cadbd75da8646835daa0eab57bbc569136c92d6
yoogodong <yoogodong@gmail.com>
2019-07-09 21:39:20 +0800

    添加 QueryDsl 支持

--- src/main/java/cn/com/ebank/customer_service/customer/CustomerRepository.java
+++ src/main/java/cn/com/ebank/customer_service/customer/CustomerRepository.java
@@ -1,12 +1,14 @@
 package cn.com.ebank.customer_service.customer;
 
 import cn.com.ebank.customer_service.customer.model.Customer;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface CustomerRepository extends CrudRepository<Customer,Long> {
+public interface CustomerRepository extends CrudRepository<Customer, Long>, QuerydslPredicateExecutor<Customer> {
     Optional<Customer> findByCustomerNumber(String customerNumber);
 
     void deleteByCustomerNumber(String customerNumber);
+
 }
